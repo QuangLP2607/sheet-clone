3ae27659bd3b248b6e4cbb8e59795754df119fe9
Lương Phúc Quang-Intern <luongquangcbvn@gmail.com>
2026-04-29 12:20:51 +0700

    deploy

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import Layout from "@/Layout";
 import Home from "@/pages/Home";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <Layout />,

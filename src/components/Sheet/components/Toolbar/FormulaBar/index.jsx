1a4d0b3bc0423d9aa01178f8e12ff5f6ad559ff7
Lương Phúc Quang-Intern <luongquangcbvn@gmail.com>
2026-04-29 21:58:06 +0700

    deploy

--- src/components/Sheet/components/Toolbar/FormulaBar/index.jsx
+++ src/components/Sheet/components/Toolbar/FormulaBar/index.jsx
@@ -8,11 +8,21 @@ import { useShallow } from "zustand/react/shallow";
 
 const cx = classNames.bind(styles);
 
+function getColumnLabel(col) {
+  let label = "";
+
+  while (col >= 0) {
+    label = String.fromCharCode((col % 26) + 65) + label;
+    col = Math.floor(col / 26) - 1;
+  }
+
+  return label;
+}
+
 export default function FormulaBar() {
   const navigate = useNavigate();
 
   const {
-    selection,
     activeCell,
     editingCell,
     editingValue,
@@ -21,7 +31,6 @@ export default function FormulaBar() {
     commitEditing,
   } = useSheetSelectionStore(
     useShallow((s) => ({
-      selection: s.selectedRange,
       activeCell: s.activeCell,
       editingCell: s.editingCell,
       editingValue: s.editingValue,
@@ -33,13 +42,12 @@ export default function FormulaBar() {
 
   const cells = useDataStore((s) => s.cells);
 
-  // ===== VALUE HIỂN THỊ =====
   let displayValue = "";
 
   if (editingCell) {
     displayValue = editingValue;
   } else if (activeCell) {
-    const key = activeCell[0] + ":" + activeCell[1];
+    const key = `${activeCell[0]}:${activeCell[1]}`;
     displayValue = cells[key] || "";
   }
 
@@ -50,22 +58,19 @@ export default function FormulaBar() {
       </button>
 
       <div className={cx("toolbar__info")}>
-        {/* ===== ADDRESS ===== */}
         <span className={cx("toolbar__info-address")}>
-          {selection &&
-            `${String.fromCharCode(65 + selection[1])}${selection[0] + 1}`}
+          {activeCell && `${getColumnLabel(activeCell[1])}${activeCell[0] + 1}`}
         </span>
 
-        {/* ===== FORMULA BAR ===== */}
         <textarea
           className={cx("toolbar__info-value")}
           value={displayValue}
           placeholder="Enter value..."
           onChange={(e) => {
-            // nếu chưa editing → bắt đầu edit cell hiện tại
             if (!editingCell && activeCell) {
               startEditing(activeCell[0], activeCell[1]);
             }
+
             setEditingValue(e.target.value);
           }}
           onKeyDown={(e) => {
